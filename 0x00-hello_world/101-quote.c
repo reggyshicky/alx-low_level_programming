9a4d91b4ee77d6b2433a08b12fcda51c376e3900
reggyshicky <reginahshikanda@gmail.com>
2023-02-16 13:20:43 -0800

    use of write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints a quoted sentence
+ *
+ * Return: Always 1(Succes)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
+	return (1);
+}
