22102d333032b618f613f163b3d6ae11ecc9249f
reggyshicky <reginahshikanda@gmail.com>
2023-02-17 06:58:53 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - lowercase in reverse
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+	c--;
+	}
+	putchar('\n');
+	return (0);
+}
