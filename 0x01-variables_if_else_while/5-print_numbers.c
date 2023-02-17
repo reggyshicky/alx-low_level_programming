040352f9ff42a229b8a2456e4bc397b478ffcdbe
reggyshicky <reginahshikanda@gmail.com>
2023-02-17 06:28:11 -0800

    0<10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Numbers between 0 and 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
