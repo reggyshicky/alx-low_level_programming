6326132c494722f0be17149b014ce23fcb5d2e77
reggyshicky <reginahshikanda@gmail.com>
2023-02-17 07:18:02 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i + '0');
+		i++;
+	}
+
+	i = 0;
+
+	while (i < 6)
+	{
+		putchar(i + 'a');
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
