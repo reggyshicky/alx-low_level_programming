50fa3953302b82c7f313134a4fac2fe0107f1948
reggyshicky <reginahshikanda@gmail.com>
2023-02-17 06:45:38 -0800

    0<10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - single numbers between 0 and 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
