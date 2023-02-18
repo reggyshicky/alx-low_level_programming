58c21823a4a7af1a123e51842f56709ec9c59683
reggyshicky <reginahshikanda@gmail.com>
2023-02-17 17:39:41 -0800

    combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Two digit combination
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0, j = 0;
+
+	while (i <= 99)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
