598519686915cb6de2ca3a8c7e454d6962a90b74
reggyshicky <reginahshikanda@gmail.com>
2023-02-21 13:32:20 -0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Adds Integers
+ * @a: Integer one
+ * @b: Integer two
+ * Return: returns the result of adding the two digits
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
