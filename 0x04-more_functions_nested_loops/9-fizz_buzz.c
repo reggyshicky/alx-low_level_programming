095844698f6733776176b974fce9cd130633d2e0
reggyshicky <reginahshikanda@gmail.com>
2023-02-23 10:47:30 -0800

    numbers

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - printing numbers
- * Return: 0 success
+ * main - Fizz Buzz code
+ *
+ * Return: void
  */
-int main()
-{
-	int i;
 
-	for (i = 1; i <= 100; i++)
+int main(void)
+{
+	int i = 1;
+
+	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%i", i);
 		}
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		i++;
 	}
-	printf("\n");
-	return 0;
+	putchar('\n');
+	return (0);
 }
