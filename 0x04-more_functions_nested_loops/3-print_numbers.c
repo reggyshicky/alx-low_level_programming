faa48115e1471f78fe44d4a4d310db69ed50de71
reggyshicky <reginahshikanda@gmail.com>
2023-02-23 04:04:52 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+# include "main.h"
+# include <stdio.h>
+/**
+ * print_numbers - print numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
