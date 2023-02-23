2c2486c7da2e44d60b66da7f3b43f7a4174fc56d
reggyshicky <reginahshikanda@gmail.com>
2023-02-23 04:33:57 -0800

    numbers ! 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_most_numbers - prints most numbers
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
