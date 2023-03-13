8c428bec2472d881de7fe8702114822b7b525876
reggyshicky <reginahshikanda@gmail.com>
2023-03-13 08:16:59 -0700

    an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include "stdlib.h"
-#include "stdio.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * create_array -pointer returned after malloc function
  * @size: size of the array
