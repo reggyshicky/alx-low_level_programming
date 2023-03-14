8a1d5b9038a618522909ee88841f67e3f4057de4
reggyshicky <reginahshikanda@gmail.com>
2023-03-14 03:07:13 -0700

    concatenate strings

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * argstostr - a pointer that concatenates strings
+ * @ac: number of arguments
+ * @av: the array of arguments
+ * Return: pointer to a new string
+ */
+char *argstostr(int ac, char **av)
+{
+	int total_len = 0; /*length of the conc string*/
+
+	int i;
+
+	char *result, *p;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		total_len += strlen(av[i]) + 1;
+	}
+	result = (char *)malloc(sizeof(char) * total_len);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	p = result;
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(p, av[i]);
+		p += strlen(av[i]);
+		*p = '\n';
+		p++;
+	}
+	*p = '\0';
+
+	return (result);
+}
+
