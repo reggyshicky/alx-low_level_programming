6d0bd1f2a4f0f566b6e98017d15492ef5cbe40aa
reggyshicky <reginahshikanda@gmail.com>
2023-03-20 05:30:05 -0700

    defining a struct alongside other prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
+/*
  * File: dog.h
  * Desc: Header file that defines a new type struct dog.
  */
 
 /**
  * struct dog - A new type describing a dog.
- * @name: The dog's name.
- * @age: The dog's age.
- * @owner: The dog's owner.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  */
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
