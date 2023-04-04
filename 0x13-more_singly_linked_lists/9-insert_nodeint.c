b2896fe721dffe8cb7f5364ba75f296a3937f58a
reggyshicky <reginahshikanda@gmail.com>
2023-04-03 23:57:19 -0700

    inserting a node at any position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newnode; /*pointer to the newly created node*/
 	unsigned int j; /*counter*/
 
-	if (head == NULL || *head == NULL)
+	if (head == NULL)
 	{
 		return (NULL);
 	}
