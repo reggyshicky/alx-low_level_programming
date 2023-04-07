74bb56f92c864b665821b5c37ecde8f5d7f3ecc0
reggyshicky <reginahshikanda@gmail.com>
2023-04-07 02:33:15 -0700

    returns the number of bits you would need to flip to get from one number to another

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,14 +8,17 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int counter_bits = 0; /*variable to rep flipped bits*/
-	unsigned long int xor; /*variable to perform XOR*/
+	unsigned long int X_O_R; /*variable to perform XOR*/
 
-	xor = n ^ m;
+	X_O_R = n ^ m;
 
-	if (X_O_R > 0)
+	while (X_O_R > 0)
 	{
-		counter_bits = counter_bits + (X_O_R & 1);
-		X_O_R >>= 1; /*discard the right most bit*/
+		if (X_O_R & 1)
+		{
+			counter_bits++;
+		}
+		X_O_R >>= 1;
 	}
 	return (counter_bits);
 }
