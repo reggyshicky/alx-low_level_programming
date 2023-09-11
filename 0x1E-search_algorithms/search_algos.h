196bd12bb05b0727fc99f608926c3b96bf2297b9
reggyshicky <reginahshikanda@gmail.com>
2023-09-11 16:47:29 +0300

    find the index of a value using binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,7 @@
 /* Prototypes */
 int binary_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
+void print_array(int *array, size_t start, size_t end);
 
 
 #endif /*SEARCH_ALGOS_H*/
