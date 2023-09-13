64e5a990deae5d91aec6be3b584a363b98ba64df
reggyshicky <reginahshikanda@gmail.com>
2023-09-14 01:18:25 +0300

    binary search recursively

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,6 +15,7 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int binary_recursion(int *array, size_t left, size_t right, int value);
 
 
 #endif /*SEARCH_ALGOS_H*/
